9a9a0cb432d6877931de9d03be38fb92060d4ad9
Chris Wright <chris.wright@nanoporetech.com>
2020-12-08 10:23:24 +0000

    Update __init__.py

--- labslauncher/__init__.py
+++ labslauncher/__init__.py
@@ -10,7 +10,7 @@ from PyQt5 import sip  # noqa: F401
 from PyQt5.QtWidgets import QMessageBox
 
 
-__version__ = "1.0.0"
+__version__ = "1.0.1"
 __UNCAUGHT__ = "Uncaught exception:"
 __LOGDIR__ = os.path.expanduser(os.path.join('~', '.labslauncher'))
 
